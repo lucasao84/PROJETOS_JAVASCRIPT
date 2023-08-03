98ad1274fef5cd74e5133b78149881ddb932956a
lucasao84 <lucasaoff84@gmail.com>
2023-08-03 18:13:44 -0300

    at

---
+++ CALCULADORA - IMC/calculardora.js
@@ -0,0 +1,9 @@
+const nome = document.getElementById('inome')
+const altura = document.getElementById('ialtura')
+const peso = document.getElementById('ipeso')
+const calcular = document.getElementById('calcular')
+const resultado = document.getElementById('resultado')
+
+
+
+nome.value = 'Lucas nolacio'
