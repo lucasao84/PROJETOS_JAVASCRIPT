8f44aaee5c6c4537f9319d3e132bc68a1665ccd5
lucasao84 <lucasaoff84@gmail.com>
2023-08-07 16:34:46 -0300

    atualizando
    repositorio

--- CALCULADORA - IMC/calculardora.js
+++ CALCULADORA - IMC/calculardora.js
@@ -10,11 +10,18 @@ const altura = document.getElementById('ialtura').value
 const peso = document.getElementById('ipeso').value
 const calcular = document.getElementById('calcular')
 
-if(nome !== '' && peso !== '' && altura !== ''){
-    alert('valor prenchido!!') 
+if( nome !== '' && peso !== '' && altura !== ''){
+    const ValorImc = (peso / (altura * altura)).toFixed(2)
+
+    let classic = ''
+
+    if(ValorImc < 18.50 ){
+        classic = 'abaixo do peso'
+    } else if(ValorImc < 25) { classic = 'com peso ideal'} else {classic = 'acima do peso'}
+    resultado.innerHTML = `Ola ${nome} o valor do seu IMC e de ${(ValorImc)} de massa corporal e voce estar ${classic}`
 } else { 
 
-resultado.innerHTML = ' prencha o campo!!'
+alert('prencha o campo abaixo!!')
 
 } }
 
